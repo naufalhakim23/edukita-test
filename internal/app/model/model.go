d61cacf66739909fe66aac0268293bcdab6e8880
naufalhakim23 <naufal.alhakim@efishery.com>
2025-04-16 23:00:48 +0700

    feat(model): improve model repo data

--- internal/app/model/model.go
+++ internal/app/model/model.go
@@ -11,8 +11,8 @@ type BaseModel struct {
 	ID        uuid.UUID  `db:"type:uuid;primary_key;default:gen_random_uuid()"`
 	CreatedBy string     `db:"created_by"`
 	CreatedAt time.Time  `db:"created_at"`
-	UpdatedBy string     `db:"updated_by"`
-	UpdatedAt time.Time  `db:"updated_at"`
-	DeletedBy string     `db:"deleted_by"`
+	UpdatedBy *string    `db:"updated_by"`
+	UpdatedAt *time.Time `db:"updated_at"`
+	DeletedBy *string    `db:"deleted_by"`
 	DeletedAt *time.Time `db:"deleted_at"`
 }
--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"net/http"
 	"time"
+
+	"edukita-teaching-grading/internal/pkg"
 
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
@@ -10,19 +13,28 @@ import (
 // User represents the common attributes for all system users
 type User struct {
 	BaseModel
-	Email        string    `db:"email" json:"email"`
-	PasswordHash string    `db:"password_hash" json:"-"`
-	FirstName    string    `db:"first_name" json:"first_name"`
-	LastName     string    `db:"last_name" json:"last_name"`
-	Role         string    `db:"role" json:"role"`
-	LastLogin    time.Time `db:"last_login" json:"last_login"`
-	IsActive     bool      `db:"is_active" json:"is_active"`
+	Email        string     `db:"email" json:"email"`
+	PasswordHash string     `db:"password_hash" json:"-"`
+	FirstName    string     `db:"first_name" json:"first_name"`
+	LastName     string     `db:"last_name" json:"last_name"`
+	Role         string     `db:"role" json:"role"`
+	LastLogin    *time.Time `db:"last_login" json:"last_login"`
+	IsActive     bool       `db:"is_active" json:"is_active"`
 }
 
 // SetPassword hashes and sets the user's password
 func (u *User) SetPassword(password string, cost int) error {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
 	if err != nil {
+		err = &pkg.AppError{
+			Code:    http.StatusText(http.StatusInternalServerError),
+			Message: err.Error(),
+			Details: []string{
+				"please check bcrypt for further details",
+			},
+			StatusCode: http.StatusInternalServerError,
+			Err:        err,
+		}
 		return err
 	}
 	u.PasswordHash = string(hash)
