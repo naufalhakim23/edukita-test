f01b8e3ed2d6bad4d0caa4b2266a4b84446601c7
naufalhakim23 <hakimnaufal212@gmail.com>
2025-04-17 20:45:19 +0700

    feat(components): add boundary not found and catch

--- web/app/router.tsx
+++ web/app/router.tsx
@@ -1,11 +1,14 @@
-// app/router.tsx
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { NotFound } from './components/NotFound'
+import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
 
 export function createRouter() {
   const router = createTanStackRouter({
     routeTree,
     scrollRestoration: true,
+    defaultErrorComponent: DefaultCatchBoundary,
+    defaultNotFoundComponent: () => <NotFound />,
   })
 
   return router
