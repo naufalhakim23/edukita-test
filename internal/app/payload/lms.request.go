84da74a00083b0354820a23a46cb3bd523b99d41
naufalhakim23 <hakimnaufal212@gmail.com>
2025-04-17 16:29:51 +0700

    fix(payload): add required tag so validation running

--- internal/app/payload/lms.request.go
+++ internal/app/payload/lms.request.go
@@ -1,34 +1,34 @@
 package payload
 
 type CreateAssignmentRequest struct {
-	CreatedBy   string  `json:"created_by"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	TotalPoints float64 `json:"total_points"`
+	CreatedBy   string  `json:"created_by" validate:"required"`
+	Title       string  `json:"title" validate:"required"`
+	Description string  `json:"description" validate:"required"`
+	TotalPoints float64 `json:"total_points" validate:"required"`
 }
 
 type UpdateAssignmentRequest struct {
-	UserID      string  `json:"user_id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	TotalPoints float64 `json:"total_points"`
-	IsPublished bool    `json:"is_published"`
+	UserID      string  `json:"user_id" validate:"required"`
+	Title       string  `json:"title" validate:"required"`
+	Description string  `json:"description" validate:"required"`
+	TotalPoints float64 `json:"total_points" validate:"required"`
+	IsPublished bool    `json:"is_published" validate:"required"`
 }
 
 type CreateSubmissionRequest struct {
-	CreatedBy    string   `json:"created_by"`
-	AssignmentID string   `json:"assignment_id"`
+	CreatedBy    string   `json:"created_by" validate:"required"`
+	AssignmentID string   `json:"assignment_id" validate:"required"`
 	Content      string   `json:"content"`
 	FileURL      string   `json:"file_url"`
 	Grade        *float64 `json:"grade"`
-	Feedback     string   `json:"feedback"`
+	Feedback     *string  `json:"feedback"`
 }
 
 type UpdateSubmissionRequest struct {
-	UserID       string   `json:"user_id"`
-	AssignmentID string   `json:"assignment_id"`
+	UserID       string   `json:"user_id" validate:"required"`
+	AssignmentID string   `json:"assignment_id" validate:"required"`
 	Content      string   `json:"content"`
 	FileURL      string   `json:"file_url"`
 	Grade        *float64 `json:"grade"`
-	Feedback     string   `json:"feedback"`
+	Feedback     *string  `json:"feedback"`
 }
